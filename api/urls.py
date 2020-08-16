0b19c61531a6ec928aacec2d19e13889fa1d6566
Shubh Agrawal <shubhagrawal1280@gmail.com>
2020-08-16 13:53:21 +0530

    docs

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('users/', include("api.users.urls")),
     path('items/', include("api.budget_app.urls")),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET', "POST"])
+def home(request):
+    return Response({"users": "users/", "items": "items/"})
